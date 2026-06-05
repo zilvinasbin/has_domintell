cac66a901e031daa5a16944a68027df027478fb8
Žilvinas <binisevicius@gmail.com>
2026-06-06 00:17:13 +0300

    fix: lights for 2026

--- custom_components/domintell/light.py
+++ custom_components/domintell/light.py
@@ -66,15 +66,8 @@ class DomintellLight(LightEntity):
         self._channel = light['channel'] - 1 # we use 0 based index internally
         self._type = light['type']
         self._state = False
-
-        if dev.is_dimmer():  # DIM / dimmable DMR module
-            self._attr_supported_color_modes = {ColorMode.BRIGHTNESS}
-            self._attr_color_mode = ColorMode.BRIGHTNESS
-        else:                  # BIR relay / convector output -> on-off only
-            self._attr_supported_color_modes = {ColorMode.ONOFF}
-            self._attr_color_mode = ColorMode.ONOFF
-
         self._brightness = None
+        
         dev = domintell.add_module(self._type, self._module)
         self._is_dimmer = dev.is_dimmer()
 
